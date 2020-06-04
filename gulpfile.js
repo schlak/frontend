a82305009d7811028372b737ee16848f9c51dbdb
Harry Merritt <harry@merritt.es>
2020-06-04 21:04:33 +0100

    chore: setup gulp to automate prettier

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require("gulp");
+const prettier = require("gulp-prettier");
+
+/*
+ * Apply prettier to all js files
+ */
+gulp.task("default", () => {
+    return gulp
+        .src([
+			"./src/**/*.js",
+			"./public/**/*.js",
+            "./package.json",
+            "./gulpfile.js",
+        ])
+        .pipe(prettier({ editorconfig: true }))
+        .pipe(gulp.dest((file) => file.base));
+});
