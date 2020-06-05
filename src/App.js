f725936b38389d86e768e41585675e05cc828bf3
Harry Merritt <harry@merritt.es>
2020-06-05 02:00:07 +0100

    display all tracks as a list
    * fetch all /tracks from api
    * link to audio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { api } from "./utils/api";
 import "./styles/index.scss";
 
 function App() {
+
+    const [tracksIndex, setTracksIndex] = useState([]);
+
+    // Fetch tracks index
+    useEffect(() => {
+        api()
+            .get("/tracks")
+            .then((res) => {
+                setTracksIndex(res.data);
+                console.log(res.data);
+            });
+    }, []);
+
     return (
         <div className="App">
-            Hello, world!
+            <div className="container" style={{margin: "50px auto"}}>
+                {
+                    tracksIndex.map((track, key) => {
+                        return (
+                            <div key={key} style={{marginTop: "10px"}}>
+                                <p>
+                                    <a href={`https://music.merritt.es/api/tracks/${track.id}/audio`} target="_blank">
+                                        {track.metadata.artist} - {track.metadata.title}
+                                    </a>
+                                </p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     );
 }
