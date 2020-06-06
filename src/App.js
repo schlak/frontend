6b68ec2886594a80814aad4d017a71e55ab11f9d
Harry Merritt <harry@merritt.es>
2020-06-06 22:06:09 +0100

    hookup track list components
    Display tracks sorted by albums

--- src/App.js
+++ src/App.js
@@ -1,37 +1,27 @@
 import React, { useState, useEffect } from "react";
+import TrackList from "./components/trackList/trackList"
 import { api } from "./utils/api";
 import "./styles/index.scss";
 
 function App() {
 
-    const [tracksIndex, setTracksIndex] = useState([]);
+    // All tracks sorted into albums
+    const [albums, setAlbums] = useState([]);
 
     // Fetch tracks index
     useEffect(() => {
         api()
-            .get("/tracks")
+            .get("/albums")
             .then((res) => {
-                setTracksIndex(res.data);
-                console.log(res.data);
+                setAlbums(res.data);
+                console.log("Albums:", res.data);
             });
     }, []);
 
     return (
         <div className="App">
             <div className="container" style={{margin: "50px auto"}}>
-                {
-                    tracksIndex.map((track, key) => {
-                        return (
-                            <div key={key} style={{marginTop: "10px"}}>
-                                <p>
-                                    <a href={`https://music.merritt.es/api/tracks/${track.id}/audio`} target="_blank">
-                                        {track.metadata.artist} - {track.metadata.title}
-                                    </a>
-                                </p>
-                            </div>
-                        )
-                    })
-                }
+                <TrackList albums={albums} />
             </div>
         </div>
     );
