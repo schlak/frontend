936ce3b90354dd49adeeb736c508ce803bb2b5ca
Harry Merritt <harry@merritt.es>
2020-06-06 23:00:38 +0100

    display track metadata
    * duration
    * title
    * artist
    * album

--- src/components/trackList/track.js
+++ src/components/trackList/track.js
@@ -1,15 +1,17 @@
 import React from "react";
+import moment from "moment";
 
 function Track({ track }) {
     return (
         <div className="track">
-            <div style={{marginTop: "10px"}}>
-                <p>
-                    <a href={`https://music.merritt.es/api/tracks/${track.id}/audio`} target="_blank" rel="noopener noreferrer">
-                        {track.metadata.artist} - {track.metadata.title}
-                    </a>
-                </p>
-            </div>
+            <a href={`https://music.merritt.es/api/tracks/${track.id}/audio`} target="_blank" rel="noopener noreferrer">
+                <div className="track-info">
+                    <p className="track-length">{moment.utc(track.metadata.duration*1000).format("mm:ss")}</p>
+                    <p className="track-title">{track.metadata.title}</p>
+                    <p className="track-artist">{track.metadata.artist}</p>
+                    <p className="track-album">{track.metadata.album}</p>
+                </div>
+            </a>
         </div>
     );
 }
