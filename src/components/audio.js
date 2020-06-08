313522f35f5f99ca31823802181fe761943e3d78
Harry Merritt <harry@merritt.es>
2020-06-08 03:23:01 +0100

    pause audio with keypress space or k
    * pause currently playing track if 'space' or 'k' key is pressed

--- src/components/audio.js
+++ src/components/audio.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Sound from "react-sound";
 
 function Audio({ audio }) {
+
+    const [playing, setPlaying] = useState(true);
+
+    // Listen for keypress
+    // Pause audio on 'space' or 'k'
+    useEffect(() => {
+        const onKeyUp = ({code}) => {
+            if (code === "Space" || code === "KeyK") {
+                setPlaying(!playing);
+            }
+        }
+
+        document.addEventListener('keyup', onKeyUp);
+        return () => {
+            document.removeEventListener('keyup', onKeyUp);
+        }
+    });
+
     return (
         <div className="audio">
             {typeof audio.playing.id === "string" &&
                 <Sound
                     url={`https://music.merritt.es/api/tracks/${audio.playing.id}/audio`}
-                    playStatus={Sound.status.PLAYING}
+                    playStatus={playing ? Sound.status.PLAYING : Sound.status.PAUSED}
                     volume={25}
                 />
             }
