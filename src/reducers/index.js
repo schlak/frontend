1017ba1521a8880c04912c96763d718f4f064e43
Harry Merritt <harry@merritt.es>
2020-06-08 23:59:28 +0100

    create action to fetch album index into redux store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,9 @@
+import {
+    FETCH_ALBUMS_START,
+    FETCH_ALBUMS_SUCCESS,
+    FETCH_ALBUMS_FAILURE,
+} from "../actions/music";
+
 // Initial state of app
 const initialState = {
     music: {
@@ -31,6 +37,46 @@ const initialState = {
 
 function musicApp(state = initialState, action) {
     switch (action.type) {
+        case FETCH_ALBUMS_START:
+            return {
+                ...state,
+                music: {
+                    ...state.music,
+                    albums: {
+                        ...state.music.albums,
+                        didError: false,
+                        isFetching: true,
+                        items: []
+                    }
+                }
+            }
+
+        case FETCH_ALBUMS_SUCCESS:
+            return {
+                ...state,
+                music: {
+                    ...state.music,
+                    albums: {
+                        ...state.music.albums,
+                        isFetching: false,
+                        items: action.payload
+                    }
+                }
+            }
+
+        case FETCH_ALBUMS_FAILURE:
+            return {
+                ...state,
+                music: {
+                    ...state.music,
+                    albums: {
+                        ...state.music.albums,
+                        didError: true,
+                        isFetching: false
+                    }
+                }
+            }
+
         default:
             return state;
     }
