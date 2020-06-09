b48633b33243e2ae283857dbf8411f62c5451347
Harry Merritt <harry@merritt.es>
2020-06-09 02:03:43 +0100

    fix app.js test by adding redux store

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,19 @@
 import React from "react";
 import { render } from "@testing-library/react";
+
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from "react-redux";
+import musicApp from '../store/reducer';
+import thunk from "redux-thunk";
+
 import App from "../App";
 
+const store = createStore(musicApp, applyMiddleware(thunk));
+
 test("renders app", () => {
-    render(<App />);
+    render(
+        <Provider store={store}>
+            <App />
+        </Provider>
+    );
 });
