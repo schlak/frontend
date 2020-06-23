07940193ca8f925037ef426b22367f7fc8072168
Harry Merritt <harry@merritt.es>
2020-06-23 18:19:42 +0100

    simplify track-list component by moving filterAlbums into separate file

--- src/components/trackList/trackList.js
+++ src/components/trackList/trackList.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import Fuse from "fuse.js";
 
 import { fetchAlbums } from "../../store/actionCreators";
+import { filterAlbums } from "../../utils/filter";
 
 import Album from "./album";
 
@@ -11,41 +11,13 @@ function TrackList() {
 
     // Get album list from store
     const albumStore = useSelector((state) => state.music.albums);
-    const filter = albumStore.filter;
-    let albums = albumStore.data;
     const isLoading = albumStore.isFetching || albumStore.didError;
 
     //// TODO: FIX track play order when filtering
 
     // Filter albums from selected tags or user input
-    albums = albumStore.data.reduce(function(filtered, album, key) {
-        // RegExp filter (case-insensitive)
-        // -> tags
-        const regexTags = new RegExp( filter.tags.join("|"), "i");
-
-        // Run search on each album
-        // Loop each track
-        if (filter.search.length > 0) {
-            const fuse = new Fuse(album.tracks, {
-                threshold: 0.2,
-                keys: ["metadata.title", "metadata.album", "metadata.artist", "metadata.album_artist", "metadata.year"]
-            });
-
-            const found = fuse.search(filter.search);
-
-            if (found.length === 0)
-                return filtered;
-        }
-
-        // If no filter applyed: add all all albums
-        // Or if album tag matches filter in RegExp
-        if (filter.tags.length === 0  ||
-            (filter.tags.length > 0 && regexTags.test(album.genre))) {
-            filtered.push(<Album albumIndex={key} key={key} />);
-        }
-
-        return filtered;
-    }, []);
+    // -> is array of album components to render
+    let albumsBeingRendered = filterAlbums(albumStore.data, albumStore.filter);
 
     // Fetch albums from api
     useEffect(() => {
@@ -60,7 +32,7 @@ function TrackList() {
                 )
             }
 
-            {albums}
+            {albumsBeingRendered}
         </div>
     );
 }
---
+++ src/utils/filter.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Fuse from "fuse.js";
+
+import Album from "../components/trackList/album";
+
+/*
+ * Filter albums from selected tags or user input
+ *
+ * @param {data}    albums array
+ * @param {filter}  filter options
+ */
+export const filterAlbums = (data, filter) => {
+    return data.reduce(function(filtered, album, key) {
+        // RegExp filter (case-insensitive)
+        // -> tags
+        const regexTags = new RegExp( filter.tags.join("|"), "i");
+
+        // Run search on each album
+        // Loop each track
+        if (filter.search.length > 0) {
+            const fuse = new Fuse(album.tracks, {
+                threshold: 0.2,
+                keys: ["metadata.title", "metadata.album", "metadata.artist", "metadata.album_artist", "metadata.year"]
+            });
+
+            const found = fuse.search(filter.search);
+
+            if (found.length === 0)
+                return filtered;
+        }
+
+        // If no filter applyed: add all all albums
+        // Or if album tag matches filter in RegExp
+        if (filter.tags.length === 0  ||
+            (filter.tags.length > 0 && regexTags.test(album.genre))) {
+            filtered.push(<Album albumIndex={key} key={key} />);
+        }
+
+        return filtered;
+    }, []);
+};
