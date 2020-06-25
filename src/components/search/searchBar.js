010cdf6e527130463c3cf5a79b692b36a8763a6c
Harry Merritt <harry@merritt.es>
2020-06-25 20:03:04 +0100

    display total tracks count in search bar placeholder

--- src/components/search/searchBar.js
+++ src/components/search/searchBar.js
@@ -7,6 +7,8 @@ function SearchBar() {
 
     // Search input value in store
     const search = useSelector((state) => state.music.tracks.filter.search);
+    let tracksLength = useSelector((state) => state.music.tracks.data.length);
+    if (tracksLength === 0) tracksLength = "loading";
 
     const updateSearch = (evt) => {
         evt.preventDefault();
@@ -25,7 +27,7 @@ function SearchBar() {
                 <path fill="currentColor" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
             </svg>
             <input
-                placeholder="Search..."
+                placeholder={`Search (${tracksLength} tracks)...`}
                 id="search"
                 name="music"
                 value={search}
