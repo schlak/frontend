cc2f84d6e4cb4d219dcb1ff92d6a92d3a17cb4e9
Harry Merritt <harry@merritt.es>
2020-06-30 22:43:21 +0100

    basic Controls component which will be used to manage audio session

---
+++ src/components/controls/controls.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { shuffleToggle } from "../../store/actionCreators";
+
+function Controls() {
+    const dispatch = useDispatch();
+
+    const isShuffle = useSelector((state) => state.session.actions.shuffle);
+
+    const handleShuffleToggle = () => {
+        dispatch(shuffleToggle());
+    };
+
+    return (
+        <div className="controls">
+            <div className={`control-button shuffle${isShuffle ? " selected" : ""}`} onClick={handleShuffleToggle}>
+                <svg viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M14.83,13.41L13.42,14.82L16.55,17.95L14.5,20H20V14.5L17.96,16.54L14.83,13.41M14.5,4L16.54,6.04L4,18.59L5.41,20L17.96,7.46L20,9.5V4M10.59,9.17L5.41,4L4,5.41L9.17,10.58L10.59,9.17Z" />
+                </svg>
+            </div>
+        </div>
+    );
+}
+
+export default Controls;
