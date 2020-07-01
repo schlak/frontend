efca2e3d33b5aa8263dc18d4b4cfeff70f504fdb
Harry Merritt <harry@merritt.es>
2020-07-01 16:33:01 +0100

    create component for each icon

---
+++ src/components/icons/previous.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function IconPrevious() {
+    return (
+        <svg viewBox="0 0 24 24">
+            <path fill="currentColor" d="M6,18V6H8V18H6M9.5,12L18,6V18L9.5,12Z" />
+        </svg>
+    );
+}
+
+export default IconPrevious;
