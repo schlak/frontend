0487a86daf816ae9bf7d9eaea73f3c69be6c3322
Harry Merritt <harry@merritt.es>
2020-07-01 17:13:05 +0100

    render skeleton for control buttons when loading

--- src/components/controls/controlButton.js
+++ src/components/controls/controlButton.js
@@ -1,9 +1,15 @@
-import React from "react";;
+import React from "react";
+import { useSelector } from "react-redux";
 
 function ControlButton({ name, icon, isActive, onClickFunction }) {
+    const trackStore = useSelector((state) => state.music.tracks);
+    const isLoading = trackStore.isFetching || trackStore.didError;
+
     return (
         <div className={`control-button ${name}${isActive ? " selected" : ""}`} onClick={onClickFunction}>
-            {icon}
+            {
+                !isLoading && icon
+            }
         </div>
     );
 }
