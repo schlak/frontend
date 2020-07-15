cd886f9a9e66393385b7cee1c1b35008b5bab101
Harry Merritt <harry@merritt.es>
2020-07-16 00:20:00 +0100

    setup Home route with required components

---
+++ src/components/RandomSelection.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function RandomSelection(props) {
+
+    return (
+        <div className="random-selection">
+            <h2>Random Selection</h2>
+        </div>
+    );
+}
+
+export default RandomSelection;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,11 +2,19 @@ import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 
+import ActiveUsers from "../components/ActiveUsers";
+import RandomSelection from "../components/RandomSelection";
+
 function Home() {
     return (
         <>
             <div className="Home container">
-                <h2>Random Selection</h2>
+                <section>
+                    <RandomSelection />
+                </section>
+                <section>
+                    <ActiveUsers />
+                </section>
             </div>
         </>
     );
