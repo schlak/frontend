900e9a94570874e84bb3f1a7d310721c53ede320
Harry Merritt <harry@merritt.es>
2020-07-16 19:35:47 +0100

    spawn a new AFCBackground on URL change

--- src/components/AFCBackgroundMulti.js
+++ src/components/AFCBackgroundMulti.js
@@ -1,26 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { isMobileOnly } from "react-device-detect";
 import { useInterval } from "../hooks/useInterval";
 
 import AFCBackground from "./AFCBackground";
 
 function AFCBackgroundMulti(props) {
-    // Spawn a new AFCBackground to slide-down
-    // on-top of current one every 20 seconds
+    // Array of AFCBackground components being rendered
     const [afcbackgrounds, setAfcbackgrounds] = useState([<AFCBackground key={0} />]);
 
+    // Spawn a new AFCBackground to slide-down
     const spawnAFCBackground = () => {
         const lastComponent = afcbackgrounds[afcbackgrounds.length-1];
         const newKey = lastComponent.key + 1;
         setAfcbackgrounds(
-            [lastComponent, <AFCBackground key={newKey} />]
+            [...afcbackgrounds.slice(-2), <AFCBackground key={newKey} />]
         );
     }
 
+    // Spawn a new AFCBackground every 30 seconds
     useInterval(() => {
         if (!isMobileOnly)
             spawnAFCBackground();
-    }, 20000);
+    }, 30000);
+
+    // Spawn a new AFCBackground on URL change
+    const location = useLocation();
+    useEffect(() => {
+        if (!isMobileOnly)
+            spawnAFCBackground();
+    }, [location]);
 
     return (<>{afcbackgrounds}</>);
 }
