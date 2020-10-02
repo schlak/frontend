7e04a71c764072d84327889a8b7bab0b871aba22
Harry Merritt <harry@merritt.es>
2020-10-02 19:28:45 +0100

    create component to find and display the most recently listened to tracks

---
+++ src/components/RecentlyListenedTracks.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { orderBy } from "lodash";
+
+import { nRowsOfAlbums } from "../utils/sortTracks";
+
+import TrackBig from "./Tracks/TrackBig";
+
+function RecentlyListenedTracks(props) {
+    // Get tracks from store
+    const tracks = useSelector((state) => state.music.tracks.data);
+    const [tracksToRender, setTracksToRender] = useState([]);
+
+    // Find 5 most recently listened to tracks
+    useEffect(() => {
+        let stats = [];
+
+        for (const i in tracks) {
+            stats.push([i, tracks[i].stats.lastPlayed]);
+        }
+
+        stats = orderBy(stats, [1], ["desc", "asc"]);
+
+        setTracksToRender(stats.slice(0, 5));
+    }, [tracks]);
+
+    return (
+        <div className="most-listened-tracks">
+            <h2>Recently Listened To</h2>
+            <div className="track-container grid grid-tracks-big">
+                {
+                    tracksToRender.map((track, index) => {
+                        return <TrackBig index={track[0]} size="big" key={index} />;
+                    })
+                }
+            </div>
+        </div>
+    );
+}
+
+export default RecentlyListenedTracks;
