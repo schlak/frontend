2a48fde73fee6f4aea77c432cf7047707dd5e86d
Harry Merritt <harry@merritt.es>
2021-10-15 20:37:47 +0100

    bump no. of albums rows to render from 3 to 4

--- src/components/TrackList/AlbumList.js
+++ src/components/TrackList/AlbumList.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 
-import { filterTracks, groupTracksIntoAlbums, nRowsOfAlbums } from "../../utils/sortTracks";
+import {
+    filterTracks,
+    groupTracksIntoAlbums,
+    nRowsOfAlbums,
+} from "../../utils/sortTracks";
 
 import Album from "../Tracks/Album";
 
@@ -15,46 +19,59 @@ function AlbumList() {
     if (trackStore.filter.length > 0) tracksData = trackStore.filteredData;
 
     // #1 Filter tracks using tags or search input
-    let tracksFiltered = filterTracks(trackStore.data, tracksData, trackStore.filter, true);
+    let tracksFiltered = filterTracks(
+        trackStore.data,
+        tracksData,
+        trackStore.filter,
+        true
+    );
 
     // #2 Populate albums from filtered array
     // #3 Create array of Album components to render
-    let albumsBeingRendered = groupTracksIntoAlbums(trackStore.data, tracksFiltered).map((data, key) => {
+    let albumsBeingRendered = groupTracksIntoAlbums(
+        trackStore.data,
+        tracksFiltered
+    ).map((data, key) => {
         return <Album album={data} key={key} />;
     });
 
     // # of rendered albums
-    const defaultRenderAmmount = nRowsOfAlbums(3);
-    const [renderedAlbumsCount, setRenderedAlbumsCount] = useState(defaultRenderAmmount);
+    const defaultRenderAmmount = nRowsOfAlbums(4);
+    const [renderedAlbumsCount, setRenderedAlbumsCount] = useState(
+        defaultRenderAmmount
+    );
 
     useEffect(() => {
         // Adds more albums as user scrolls to bottom of page
         const handleScroll = () => {
-            if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - (window.innerHeight + 600))
-            {
+            if (
+                window.innerHeight + window.scrollY >=
+                document.body.offsetHeight - (window.innerHeight + 600)
+            ) {
                 if (renderedAlbumsCount < albumsBeingRendered.length) {
                     // Add album to render
-                    setRenderedAlbumsCount(renderedAlbumsCount + nRowsOfAlbums(3));
+                    setRenderedAlbumsCount(
+                        renderedAlbumsCount + nRowsOfAlbums(4)
+                    );
                 }
             } else if (window.scrollY <= 100) {
                 // Reset render count
                 setRenderedAlbumsCount(defaultRenderAmmount);
             }
-        }
+        };
 
         window.addEventListener("scroll", handleScroll);
         return () => {
             window.removeEventListener("scroll", handleScroll);
-        }
+        };
     });
 
     return (
         <div className="track-container grid grid-albums">
             {isLoading &&
-                [...Array(nRowsOfAlbums(3))].map((x, key) =>
+                [...Array(nRowsOfAlbums(4))].map((x, key) => (
                     <Album album={false} key={key} />
-                )
-            }
+                ))}
 
             {albumsBeingRendered.slice(0, renderedAlbumsCount)}
         </div>
