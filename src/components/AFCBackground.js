c0d2b9013d1b8569ea42688afeaf5399b2ab0f95
Harry Merritt <harry@merritt.es>
2021-10-17 18:19:20 +0100

    modify AFCBackground to use the global color
    * This also makes each background-color unique

--- src/components/AFCBackground.js
+++ src/components/AFCBackground.js
@@ -1,19 +1,7 @@
 import React from "react";
 import { useSpring, animated } from "react-spring";
 
-function AFCBackground(props) {
-    const colors = [
-        "#d5f1ff",
-        "#cdfff5",
-        "#cdffda",
-        "#fcffcd",
-        "#ffeecd",
-        "#ffcde8",
-        "#fccdff",
-        "#d5e0ff",
-    ];
-    const color = colors[Math.floor(Math.random() * colors.length)];
-
+function AFCBackground({ color }) {
     const styles = useSpring({
         from: { height: 0, opacity: 0, backgroundColor: "#ffffff" },
         to: { height: 420, opacity: 0.5, backgroundColor: color },
