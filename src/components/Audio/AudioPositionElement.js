7a08edf01e16155e9db96dc6431f8282095a02ac
hmerritt <harry@mrrtt.me>
2022-03-05 14:31:43 +0000

    wip seek hitbox

--- src/components/Audio/AudioPositionElement.js
+++ src/components/Audio/AudioPositionElement.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import { isMobile } from "react-device-detect";
 
 function AudioPositionElement(props) {
+    const hitboxRef = useRef(null);
+    const audioRef = useSelector((state) => state.session.playing.audioRef);
     const trackStatus = useSelector((state) => state.session.playing.status);
+
     let statusCompletedPercentage = 0;
 
     // Calculate progress bar percentage
     if (typeof trackStatus.duration === "number") {
-        statusCompletedPercentage = (trackStatus.position / trackStatus.duration) * 100;
+        statusCompletedPercentage =
+            (trackStatus.position / trackStatus.duration) * 100;
 
         // Reduce # of DOM changes on mobile/tablet by
         // flooring result to an integer
@@ -17,12 +21,27 @@ function AudioPositionElement(props) {
         }
     }
 
+    useEffect(() => {
+        console.log(audioRef?.current);
+    }, []);
+
+    const handleHitbox = (e) => {
+        console.log(hitboxRef.current);
+    };
+
     return (
-        <div
-            className="audio-position-element"
-            style={{width: `${statusCompletedPercentage}%`}}
-        >
-        </div>
+        <>
+            <div
+                className="audio-position-element"
+                style={{ width: `${statusCompletedPercentage}%` }}
+            ></div>
+
+            <div
+                ref={hitboxRef}
+                onClick={handleHitbox}
+                className="audio-position-selection-hitbox"
+            ></div>
+        </>
     );
 }
 
