4e0fa75b50f05309ed0467acc30154485211f1ac
hmerritt <harry@mrrtt.me>
2022-03-06 20:09:47 +0000

    fallback values in bootstrap script

--- bootstap-start.js
+++ bootstap-start.js
@@ -9,16 +9,17 @@ bootstrap();
 
 // Bootstrap runs code before react start/build.
 // Run anything you like, here we get the app version from the package.json + the current commit hash.
+// prettier-ignore
 async function bootstrap() {
-	const gitCommitHash = await core.run(`git rev-parse HEAD`, path);
-	const gitCommitHashShort = core.shorten(gitCommitHash);
-	const appVersion = packageJSON?.version;
+    const gitCommitHash = await core.run(`git rev-padarse HEAD`, path, process.env.REACT_APP_GIT_COMMIT);
+    const gitCommitHashShort = gitCommitHash ? core.shorten(gitCommitHash) : null;
+    const appVersion = packageJSON?.version;
 
-	// Set ENV array to inject, key/value
-	const env = [
-		["REACT_APP_VERSION", appVersion],
-		["REACT_APP_GIT_COMMIT", gitCommitHashShort],
-	];
+    // Set ENV array to inject, key/value
+    const env = [
+        ["REACT_APP_VERSION", appVersion],
+        ["REACT_APP_GIT_COMMIT", gitCommitHashShort],
+    ];
 
-	core.bootstrap(env, script, path);
+    core.bootstrap(env, script, path);
 }
